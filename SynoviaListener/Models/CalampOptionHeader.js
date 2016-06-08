807758eb333d0a64f6088e162ac2493f1a8fa337
William Davis <william.j.davis5@gmail.com>
2016-06-08 10:02:26 -0400

    asdf

---
+++ SynoviaListener/Models/CalampOptionHeader.js
@@ -0,0 +1,29 @@
+﻿function CalampOptionHeader() {
+    this.MobileIdTypeLength = 1;
+    this.HeaderBytes = [];
+    this.MessagePointer = 0;
+    this.AuthenticationFlag = false;
+    this.AuthenticationLength = 0;
+    this.AuthenticationWord = "";
+    this.ForwardingAddress = "";
+    this.ForwardingFlag = false;
+    this.ForwardingLength = 0;
+    this.ForwardingOperation = 0;
+    this.ForwardingPointer = 0;
+    this.ForwardingPort = 0;
+    this.ForwardingProtocol = 17;
+    this.HeaderFlag = false;
+    this.HeaderValue = 0;
+    this.MobileId = "";
+    this.MobileIdFlag = false;
+    this.MobileIdLength = 0;
+    this.MobileIdType = 0;//Off = 0,MobileId = 1,Imei = 2,Imsi = 3,UserDefined = 4,PhoneNumber = 5,IpAddress = 6,
+    this.MobileIdTypeFlag = false;
+    this.RoutingField = "";
+    this.RoutingFlag = false;
+    this.RoutingLength = 0;
+}
+
+CalampOptionHeader.prototype.Parse = function (msgBytes){
+    
+}
